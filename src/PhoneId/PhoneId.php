e685e830e67769b66db8fbd5ddc5ba4aa219e0c5
anegro <a.negro@wirex.it>
2015-08-02 11:27:01 +0200

    Fix header issue
    Fix header issue

--- src/PhoneId/PhoneId.php
+++ src/PhoneId/PhoneId.php
@@ -127,8 +127,7 @@ class PhoneId
 
         // Prepare headers
         if ($this->_accessToken) {
-            // TODO $headers['Authorization'] = 'Bearer ' . $this->_accessToken;
-            $data['access_token'] = $this->_accessToken;
+            $headers[] = 'Authorization: Bearer ' . $this->_accessToken;
         }
 
         // Curl options
