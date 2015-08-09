ffe9295a633ff1b91e1bc3216592163986f6f019
Marco Pracucci <marco.pracucci@spreaker.com>
2015-08-09 07:38:39 +0200

    Basic unit tests

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ To register an app and get Phone.id credentials, you should:
 
 1. Login to https://developer.phone.id with your mobile phone
 2. Create a new app and get client id and secret
+
+
+### Contributing
+
+Run tests before pushing a pull request:
+
+``` bash
+phpunit --bootstrap src/PhoneId/autoload.php tests
+```
---
+++ tests/PhoneId/PhoneIdTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PhoneId\PhoneId;
+
+
+class PhoneIdTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testGetAuthorizeUrl()
+    {
+        $p = new PhoneId(123, 'secret', array('redirect_uri' => 'http://localhost/return.php'));
+
+        $expected = 'https://api.phone.id/v2/static/login.html?client_id=123&redirect_uri=' . urlencode('http://localhost/return.php');
+        $actual   = $p->getAuthorizeUrl();
+        $this->assertEquals($expected, $actual, 'should build the authorize URL with client_id and redirect_uri');
+
+        $expected = 'https://api.phone.id/v2/static/login.html?client_id=123&redirect_uri=' . urlencode('http://www.domain.com/return.php');
+        $actual   = $p->getAuthorizeUrl(array('redirect_uri' => 'http://www.domain.com/return.php'));
+        $this->assertEquals($expected, $actual, 'should override query string parameters from input');
+
+        $expected = 'https://api.phone.id/v2/static/login.html?client_id=123';
+        $actual   = $p->getAuthorizeUrl(array('redirect_uri' => null));
+        $this->assertEquals($expected, $actual, 'should override and remove null query string parameters');
+
+        $expected = 'https://api.phone.id/v2/static/login.html?client_id=123&redirect_uri=' . urlencode('http://localhost/return.php') . '&extra=value';
+        $actual   = $p->getAuthorizeUrl(array('extra' => 'value'));
+        $this->assertEquals($expected, $actual, 'should append query string parameters from input');
+
+    }
+
+}
